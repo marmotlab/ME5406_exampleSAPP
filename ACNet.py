0d8c0d024a8b749de7347f9437d6f9a5ba8b6446
Guillaume Sartoretti <guillaume.sartoretti@gmail.com>
2021-10-15 20:24:28 +0800

    Streamlined ACNet for better readability

--- ACNet.py
+++ ACNet.py
@@ -22,10 +22,20 @@ class ACNet:
     def __init__(self, scope, a_size, trainer, TRAINING, GRID_SIZE):
         with tf.variable_scope(str(scope)+'/qvalues'):
             #The input size may require more work to fit the interface.
-            self.inputs = tf.placeholder(shape=[None,3,GRID_SIZE,GRID_SIZE], dtype=tf.float32)
-            self.goal_pos=tf.placeholder(shape=[None,3],dtype=tf.float32)
-            self.myinput = tf.transpose(self.inputs, perm=[0,2,3,1])
-            self.policy, self.value, self.state_out, self.state_in, self.state_init, self.valids = self._build_net(self.myinput,self.goal_pos,RNN_SIZE,a_size)
+            self.inputs   = tf.placeholder(shape=[None,3,GRID_SIZE,GRID_SIZE], dtype=tf.float32)
+            self.goal_pos = tf.placeholder(shape=[None,3],dtype=tf.float32)
+            self.myinput  = tf.transpose(self.inputs, perm=[0,2,3,1])
+            
+            # Define initial and current LSTM states
+            c_init = np.zeros((1, RNN_SIZE), np.float32) # 1x128 full of zeros
+            h_init = np.zeros((1, RNN_SIZE), np.float32) # 1x128 full of zeros
+            self.state_init = [c_init, h_init]
+            c_in = tf.placeholder(tf.float32, [1, RNN_SIZE]) # 1x128, current state/memory
+            h_in = tf.placeholder(tf.float32, [1, RNN_SIZE]) # 1x128, current state/memory
+            #self.state_in = (c_in, h_in)
+            self.state_in = tf.nn.rnn_cell.LSTMStateTuple(c_in, h_in)
+
+            self.policy, self.value, self.state_out, self.valids = self._build_net(self.myinput,self.goal_pos,RNN_SIZE,a_size)
 
         if TRAINING:
             self.actions                = tf.placeholder(shape=[None], dtype=tf.int32) # [a_1, a_2, ..., a_T], ex [2,3]
@@ -56,49 +66,41 @@ class ACNet:
         w_init   = layers.variance_scaling_initializer()
         
         # input: 11x11x3
-        # goal_vec = 1x3
+        # goal_pos = 1x3
         
-        conv1    =  layers.conv2d(inputs=inputs,   padding="SAME", num_outputs=RNN_SIZE//4,  kernel_size=[3, 3],   stride=1, data_format="NHWC", weights_initializer=w_init,activation_fn=tf.nn.relu) # 11x11x32
-        conv1a   =  layers.conv2d(inputs=conv1,   padding="SAME", num_outputs=RNN_SIZE//4,  kernel_size=[3, 3],   stride=1, data_format="NHWC", weights_initializer=w_init,activation_fn=tf.nn.relu) # 11x11x32
-        conv1b   =  layers.conv2d(inputs=conv1a,   padding="SAME", num_outputs=RNN_SIZE//4,  kernel_size=[3, 3],   stride=1, data_format="NHWC", weights_initializer=w_init,activation_fn=tf.nn.relu) # 11x11x32
-        pool1    =  layers.max_pool2d(inputs=conv1b,kernel_size=[2,2]) # 5x5x32
+        conv1    =  layers.conv2d(inputs=inputs,   padding="SAME", num_outputs=RNN_SIZE//4,  kernel_size=[3, 3],   stride=1, data_format="NHWC", weights_initializer=w_init, activation_fn=tf.nn.relu) # 11x11x32
+        conv1a   =  layers.conv2d(inputs=conv1,   padding="SAME", num_outputs=RNN_SIZE//4,  kernel_size=[3, 3],   stride=1, data_format="NHWC", weights_initializer=w_init, activation_fn=tf.nn.relu) # 11x11x32
+        conv1b   =  layers.conv2d(inputs=conv1a,   padding="SAME", num_outputs=RNN_SIZE//4,  kernel_size=[3, 3],   stride=1, data_format="NHWC", weights_initializer=w_init, activation_fn=tf.nn.relu) # 11x11x32
+        pool1    =  layers.max_pool2d(inputs=conv1b, kernel_size=[2,2]) # 5x5x32
 
         conv2    =  layers.conv2d(inputs=pool1,    padding="SAME", num_outputs=RNN_SIZE//2, kernel_size=[3, 3],   stride=1, data_format="NHWC", weights_initializer=w_init,activation_fn=tf.nn.relu) # 5x5x64
         conv2a   =  layers.conv2d(inputs=conv2,    padding="SAME", num_outputs=RNN_SIZE//2, kernel_size=[3, 3],   stride=1, data_format="NHWC", weights_initializer=w_init,activation_fn=tf.nn.relu) # 5x5x64
         conv2b   =  layers.conv2d(inputs=conv2a,    padding="SAME", num_outputs=RNN_SIZE//2, kernel_size=[3, 3],   stride=1, data_format="NHWC", weights_initializer=w_init,activation_fn=tf.nn.relu) # 5x5x64
-        pool2    =  layers.max_pool2d(inputs=conv2b,kernel_size=[2,2])   # 2x2x64
+        pool2    =  layers.max_pool2d(inputs=conv2b, kernel_size=[2,2])   # 2x2x64
         conv3    =  layers.conv2d(inputs=pool2,    padding="VALID", num_outputs=RNN_SIZE-GOAL_REPR_SIZE, kernel_size=[2, 2],   stride=1, data_format="NHWC", weights_initializer=w_init,activation_fn=None)       # 1x1x116
 
-        flat     = tf.nn.relu(layers.flatten(conv3)) # 1x116
-        goal_layer= layers.fully_connected(inputs=goal_pos, num_outputs=GOAL_REPR_SIZE) # 1x12
-        hidden_input=tf.concat([flat,goal_layer],1) # # 1x116 + # 1x12 = # 1x128
-        h1 = layers.fully_connected(inputs=hidden_input,  num_outputs=RNN_SIZE) # 1x128
-        h2 = layers.fully_connected(inputs=h1,  num_outputs=RNN_SIZE, activation_fn=None) # 1x128
+        flat         = tf.nn.relu(layers.flatten(conv3)) # 1x116
+        goal_layer   = layers.fully_connected(inputs=goal_pos, num_outputs=GOAL_REPR_SIZE) # 1x12 (WITH ReLU)
+        hidden_input = tf.concat([flat, goal_layer],1) # # 1x116 + # 1x12 = # 1x128
+        h1 = layers.fully_connected(inputs=hidden_input,  num_outputs=RNN_SIZE) # 1x128 (WITH ReLU)
+        h2 = layers.fully_connected(inputs=h1,  num_outputs=RNN_SIZE, activation_fn=None) # 1x128 (WITHOUT ReLU)
         self.h3 = tf.nn.relu(h2 + hidden_input) # residual shortcut, # 1x128
 
         #Recurrent network for temporal dependencies
         lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(RNN_SIZE,state_is_tuple=True)
-        c_init = np.zeros((1, lstm_cell.state_size.c), np.float32) # 1x128 full of zeros
-        h_init = np.zeros((1, lstm_cell.state_size.h), np.float32) # 1x128 full of zeros
-        state_init = [c_init, h_init]
-
-        c_in = tf.placeholder(tf.float32, [1, lstm_cell.state_size.c]) # 1x128, current state/memory
-        h_in = tf.placeholder(tf.float32, [1, lstm_cell.state_size.h]) # 1x128, current state/memory
-        state_in = (c_in, h_in)
 
         rnn_in = tf.expand_dims(self.h3, [0])
         step_size = tf.shape(inputs)[:1]
-        state_in = tf.nn.rnn_cell.LSTMStateTuple(c_in, h_in)
         lstm_outputs, lstm_state = tf.nn.dynamic_rnn(
-        lstm_cell, rnn_in, initial_state=state_in, sequence_length=step_size,
-        time_major=False)
+            lstm_cell, rnn_in, initial_state=self.state_in, sequence_length=step_size,
+            time_major=False) # THE lstm operation (rnn_in (h3) "+" current LSTM state (state_in) --> lstm_outputs, new state (lstm_state)
         lstm_c, lstm_h = lstm_state
         state_out = (lstm_c[:1, :], lstm_h[:1, :]) # new LSTM state/memory
-        self.rnn_out = tf.reshape(lstm_outputs, [-1, RNN_SIZE]) # output of the LSTM (from h3 + LSTM state)
+        self.rnn_out = tf.reshape(lstm_outputs, [-1, RNN_SIZE]) # output of the LSTM (from h3 "+" LSTM state) # 1x128
 
-        policy_layer = layers.fully_connected(inputs=self.rnn_out, num_outputs=a_size,weights_initializer=normalized_columns_initializer(1./float(a_size)), biases_initializer=None, activation_fn=None)
-        policy       = tf.nn.softmax(policy_layer) # 1x5 of action activation (normalized probability vector)
-        policy_sig   = tf.sigmoid(policy_layer)
-        value        = layers.fully_connected(inputs=self.rnn_out, num_outputs=1, weights_initializer=normalized_columns_initializer(1.0), biases_initializer=None, activation_fn=None)
+        policy_layer = layers.fully_connected(inputs=self.rnn_out, num_outputs=a_size, weights_initializer=normalized_columns_initializer(1./float(a_size)), biases_initializer=None, activation_fn=None) # [w1 w2 w3 w4 w5]
+        policy       = tf.nn.softmax(policy_layer) # 1x5 of action activation (normalized probability vector) # [p1 p2 p3 p4 p5] sum(pi) = 1
+        policy_sig   = tf.sigmoid(policy_layer)                                                              # [b1 b2 b3 b4 b5] bi \in {0,1}
+        value        = layers.fully_connected(inputs=self.rnn_out, num_outputs=1, weights_initializer=normalized_columns_initializer(1.0), biases_initializer=None, activation_fn=None) # 1x1
 
-        return policy, value, state_out, state_in, state_init, policy_sig
+        return policy, value, state_out, policy_sig
